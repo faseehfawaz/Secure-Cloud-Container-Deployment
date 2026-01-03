3e0091dcbf8f3934ee275b429c1da209967ffd21
Faseeh Padinjarathil <faseehp@outlook.com>
2026-01-03 17:00:19 +0000

    Fix B105: Replace hardcoded password with env var

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
-# WARNING: This is bad code on purpose.
-# Hardcoded passwords are a major security risk.
+import os
 
 def connect_to_database():
+    # Get password from Environment Variable.
+    # If not found, use "None" (but never a real password).
     username = "admin"
-    password = "superSecretPassword123"  # Bandit should catch this!
-    print(f"Connecting as {username}")
+    password = os.environ.get("DB_PASSWORD")
 
-connect_to_database()
+    if password:
+        print(f"Connecting as {username}")
+    else:
+        print("Error: DB_PASSWORD variable not set!")
+
+if __name__ == "__main__":
+    connect_to_database()
